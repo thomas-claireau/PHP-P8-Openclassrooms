50f49e003faf012c52b5303bfb99b1432f2ca0a7
Thomas Claireau <tclaireau@kookline.net>
2020-03-13 08:11:47 +0100

    security controller test OK

--- tests/Unit/Controller/SecurityControllerTest.php
+++ tests/Unit/Controller/SecurityControllerTest.php
@@ -57,69 +57,67 @@ class SecurityControllerTest extends WebTestCase
 	}
 
 	/**
-	 * Test wrong username when login
+	 * Test wrong credentials when login
 	 * 
 	 * @return void
 	 */
-	// public function testWrongUsernameWhenLogin()
-	// {
-	// }
+	public function testWrongCredientialsWhenLogin()
+	{
+		$this->client->request('POST', '/login_check', ['_username' => 'xxx', '_password' => 'xxx']);
+		$crawler = $this->client->followRedirect();
+		$this->assertStringContainsString('Invalid credentials.', $crawler->text());
+	}
 
 	/**
-	 * Test incorrect format username when login
+	 * Test login with correct params (eq. admin)
 	 * 
 	 * @return void
 	 */
-	// public function testIncorrectFormatUsernameWhenLogin()
-	// {
-	// }
-
-	/**
-	 * Test wrong password when login
-	 * 
-	 * @return void
-	 */
-	// public function testWrongPasswordWhenLogin()
-	// {
-	// }
-
-	/**
-	 * Test login with correct params
-	 * 
-	 * @return void
-	 */
-	// public function testLoginWithCorrectParams()
-	// {
-	// }
+	public function testLoginWithCorrectParams()
+	{
+		$this->client->request('POST', '/login_check', ['_username' => 'root', '_password' => 'root']);
+		$crawler = $this->client->followRedirect();
+		$this->assertStringContainsString('Créer une nouvelle tâche', $crawler->text());
+	}
 
 	/**
 	 * Test access user management with user role
 	 * 
 	 * @return void
 	 */
-	// public function testAccessUserManagementWithUserRole()
-	// {
-	// }
+	public function testAccessUserManagementWithUserRole()
+	{
+		$this->logIn('user');
+		$this->client->request('GET', '/users');
+		$this->assertEquals('302', $this->client->getResponse()->getStatusCode());
+	}
 
 	/**
 	 * Test access user management with admin role
 	 * 
 	 * @return void
 	 */
-	// public function testAccessUserManagementWithAdminRole()
-	// {
-	// }
+	public function testAccessUserManagementWithAdminRole()
+	{
+		$this->logIn('admin');
+		$this->client->request('GET', '/users');
+		$this->assertEquals('200', $this->client->getResponse()->getStatusCode());
+	}
 
 	/**
 	 * Test logout
 	 * 
 	 * @return void
 	 */
-	// public function testLogout()
-	// {
-	// }
+	public function testLogout()
+	{
+		$this->logIn('admin');
+		$this->client->request('GET', '/logout');
+		$this->client->request('GET', '/tasks');
+		$this->assertEquals('302', $this->client->getResponse()->getStatusCode());
+	}
 
-	public function logIn($isAdmin)
+	private function logIn($isAdmin)
 	{
 		$credentials = $isAdmin == 'admin' ? self::ADMIN : self::USER;
 
