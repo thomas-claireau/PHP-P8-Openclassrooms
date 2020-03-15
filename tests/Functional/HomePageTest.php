8919316fa7c5d0313dd17af9c6a0dc31aa0609b5
Thomas Claireau <thomas.claireau@gmail.com>
2020-03-15 12:35:07 +0100

    Home page test OK

--- tests/Functional/HomePageTest.php
+++ tests/Functional/HomePageTest.php
@@ -2,34 +2,76 @@
 
 namespace App\Tests\Functional;
 
+use App\Tests\LogUtils;
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomePageTest extends WebTestCase
 {
+	private $client;
+	private $logUtils;
+
+	public function setUp(): void
+	{
+		$this->client = static::createClient();
+		$this->logUtils = new LogUtils($this->client);
+	}
 	/**
-	 * Test access add task button
+	 * Test access add task button and check uri
 	 * 
 	 * @return void
 	 */
-	// public function testAccessAddTaskButton()
-	// {
-	// }
+	public function testAccessAddTaskButton()
+	{
+		$this->logUtils->login('user');
+		$crawler = $this->client->request('GET', '/');
+		$linkAddTask = $crawler->selectLink("Créer une nouvelle tâche")->link()->getUri();
+		$crawler = $this->client->request('GET', $linkAddTask);
+
+		$this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+		$titlePage = $crawler->filter('h1')->text();
+		$this->assertStringContainsString("Créer une tâche", $titlePage);
+	}
 
 	/**
 	 * Test access all tasks button
 	 * 
 	 * @return void
 	 */
-	// public function testAccessAllTasksButton()
-	// {
-	// }
+	public function testAccessAllTasksButton()
+	{
+		$this->logUtils->login('user');
+		$crawler = $this->client->request('GET', '/');
+		$linkViewTasks = $crawler->selectLink("Consulter la liste des tâches à faire")->link()->getUri();
+
+		$crawler = $this->client->request('GET', $linkViewTasks);
+
+		$this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+		$task = $crawler->filter('.task')->text();
+		$this->assertNotEquals(null, $task);
+	}
 
 	/**
 	 * Test access all tasks done button
 	 * 
 	 * @return void
 	 */
-	// public function testAccessAllTasksDoneButton()
-	// {
-	// }
+	public function testAccessAllTasksDoneButton()
+	{
+		$this->logUtils->login('user');
+		$crawler = $this->client->request('GET', '/');
+		$linkViewDoneTasks = $crawler->selectLink("Consulter la liste des tâches terminées")->link()->getUri();
+
+		$crawler = $this->client->request('GET', $linkViewDoneTasks);
+
+		$this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+		$crawler->filter('.caption div.toggle')->each(function (Crawler $node, $i) {
+			$taskToggle = filter_var($node->attr('data-is-done'), FILTER_VALIDATE_BOOLEAN);
+
+			$this->assertEquals(true, $taskToggle);
+		});
+	}
 }
