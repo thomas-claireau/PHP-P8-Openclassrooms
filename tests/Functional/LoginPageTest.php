24bc47c425aa9a3b8b1e6de5bf69679ad73ba3df
Thomas Claireau <thomas.claireau@gmail.com>
2020-03-20 10:51:27 +0100

    Login page test OK

--- tests/Functional/LoginPageTest.php
+++ tests/Functional/LoginPageTest.php
@@ -2,25 +2,59 @@
 
 namespace App\Tests\Functional;
 
+use App\Tests\LogUtils;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class LoginPageTest extends WebTestCase
 {
+	private $client;
+	private $logUtils;
+
+	public function setUp(): void
+	{
+		$this->client = static::createClient();
+		$this->logUtils = new LogUtils($this->client);
+	}
+
 	/**
 	 * Test submit wrong authentication
 	 * 
 	 * @return void
 	 */
-	// public function testSubmitWrongAuthentication()
-	// {
-	// }
+	public function testSubmitWrongAuthentication()
+	{
+		$crawler = $this->client->request('GET', '/login');
+		$loginForm = $crawler->selectButton("Se connecter")->form();
+
+		$this->assertNotEquals(null, $loginForm);
+
+		$loginForm['_username'] = 'xxx';
+		$loginForm['_password'] = 'xxx';
+
+		$crawler = $this->client->submit($loginForm);
+		$crawler = $this->client->followRedirect();
+
+		$this->assertStringContainsString('Invalid credentials.', $crawler->text());
+	}
 
 	/**
 	 * Test submit correct authentication
 	 * 
 	 * @return void
 	 */
-	// public function testSubmitCorrectAuthentication()
-	// {
-	// }
+	public function testSubmitCorrectAuthentication()
+	{
+		$crawler = $this->client->request('GET', '/login');
+		$loginForm = $crawler->selectButton("Se connecter")->form();
+
+		$this->assertNotEquals(null, $loginForm);
+
+		$loginForm['_username'] = 'admin';
+		$loginForm['_password'] = 'admin';
+
+		$crawler = $this->client->submit($loginForm);
+		$crawler = $this->client->followRedirect();
+
+		$this->assertStringContainsString('Créer une nouvelle tâche', $crawler->text());
+	}
 }
