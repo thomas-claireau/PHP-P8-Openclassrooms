21f3340a1257dcf5f851aab2be74517f6a9dc3ce
Thomas Claireau <thomas.claireau@gmail.com>
2020-03-20 17:40:04 +0100

    add phpinfo page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -13,5 +14,18 @@ class DefaultController extends AbstractController
 	public function indexAction()
 	{
 		return $this->render('default/index.html.twig');
+	}
+
+	/**
+	 * @Route("/phpinfo", name="phpinfo")
+	 */
+	public function phpinfo()
+	{
+		ob_start();
+		phpinfo();
+		$str = ob_get_contents();
+		ob_get_clean();
+
+		return new Response($str);
 	}
 }
